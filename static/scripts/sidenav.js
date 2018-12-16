09162fc492f90e46774d84c95a2756441d555708
Justin Richmond-Decker <richmonddecker@gmail.com>
2018-12-15 20:28:48 -0700

    Got a pretty cool sidebar working, and started using video tag.

--- static/scripts/sidenav.js
+++ static/scripts/sidenav.js
@@ -1,7 +1,17 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.opacity = "1";
+  document.getElementById("myTab").style.left = "250px";
+  document.getElementById("myTab").style.opacity = "0";
+  document.getElementById("myTab").style.width = "0";
+  document.getElementById("myArrow").style.visibility = "hidden";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "30px";
+  document.getElementById("mySidenav").style.opacity = "0";
+  document.getElementById("myTab").style.left = "0";
+  document.getElementById("myTab").style.opacity = "1";
+  document.getElementById("myTab").style.width = "30px";
+  document.getElementById("myArrow").style.visibility = "visible";
 }
